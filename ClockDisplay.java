eb0a6e7d38dbaede3b81afc8e371d187a1527ce1
miguel <miganrobles@gmail.com>
2015-11-20 19:15:03 +0100

    Modificada la clase clockDisplay para que nos añada la fecha a nuestro display

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -22,49 +22,74 @@ public class ClockDisplay
     private NumberDisplay minutos;
     // Atributo donde almacenar la hora actual
     private String horaActual;
-    // Atributo para definir si el reloj es de 24 o de 12 horas
+    // Atributo para definir si el reloj tiene formato de 24 o de 12 horas.
     private boolean formatoHora;
-    
+    // Atributo para el año de nuestro display
+    private NumberDisplay year;
+    // Atributo para el mes de nuestro display
+    private NumberDisplay mes;
+    // Atributo para el dia de nuestro display
+    private NumberDisplay dia;
+ 
     /**
      * Constructor que fija la hora a "00:00" y nos permite elejir si queremos un reloj
-     * en formato de 24 o de 12 horas(si el valor es false funciona como 24 y si es true en formato de 12 horas).
+     * en formato de 24 o de 12 horas(si el valor es false el reloj tendra el formato de 24 horas y si es true tendrá formato de 12 horas).
+     * Además nos añade la fecha de hoy en formato de dos digitos para el día, dos para el mes y dos para el año (dd-mm-aa).
      */
     public ClockDisplay(boolean formatoH)
     {
         horas = new NumberDisplay(24);
         minutos = new NumberDisplay(60);
+        year = new NumberDisplay(100);
+        mes = new NumberDisplay(13);
+        dia = new NumberDisplay(31);
         formatoHora = formatoH;
+        year.setValue(15);
+        mes.setValue(11);
+        dia.setValue(20);
         modificarDisplay();
-        
     }
     
     /**
      * Constructor que representan las horas y los minutos a los que fijar la hora actual y nos permite elejir si queremos un reloj
-     * en formato de 24 o de 12 horas(si el valor es false funciona como 24 y si es true en formato de 12 horas).
+     * en formato de 24 o de 12 horas(si el valor es false el reloj tendra el formato de 24 horas y si es true tendrá formato de 12 horas).
+     * Además nos permmite introducir la fecha que deseemos en formato de dos digitos para el día, dos para el mes y dos para el año (dd-mm-aa).
      */ 
-    public ClockDisplay(int horaAct, int minutosAct, boolean formatoH)
+    public ClockDisplay(int horaAct, int minutosAct, boolean formatoH, int confYear, int confMes, int confDia)
     {
         horas = new NumberDisplay(24);
         minutos = new NumberDisplay(60);
-        horas.setValue(horaAct);
-        minutos.setValue(minutosAct);
+        year = new NumberDisplay(100);
+        mes = new NumberDisplay(13);
+        dia = new NumberDisplay(31);
+        horas.setValue(horaAct); 
+        minutos.setValue(minutosAct); 
         formatoHora = formatoH;
+        year.setValue(confYear);
+        mes.setValue(confMes);
+        dia.setValue(confDia);
         modificarDisplay();
     }
     
     /**
      * Método setTime que acepta dos parámetros de tipo int, que representan horas y minutos,
      * y que fija dichos valores como el tiempo actual del reloj.
+     * También acepta tres parámetros de tipo int, que representan la fecha en formato (dd-mm-aa) para poder
+     * cambiar la fecha anterior de nuestro reloj.
      */
-    public void setTime(int horaAc, int minutosAc)
+    public void setTime(int changeHoras, int changeMinutos, int changeYear, int changeMes, int changeDia)
     {
-        horas.setValue(horaAc);
-        minutos.setValue(minutosAc);
+        horas.setValue(changeHoras);
+        minutos.setValue(changeMinutos);
+        year.setValue(changeYear);
+        mes.setValue(changeMes);
+        dia.setValue(changeDia);
         modificarDisplay();
     }
     
     /**
-     * Método getTime que devuelva la hora como String de 5 caracteres.
+     * Método getTime que devuelva la hora como String con el formato adecuado según se haya elegido
+     * en el constructor del reloj.
      */
     public String getTime()
     {
@@ -72,42 +97,52 @@ public class ClockDisplay
     }
     
     /**
-     * Método timeTick que hace avanzar un minuto la hora actual.
+     * Método timeTick que hace avanzar un minuto la hora actual y realiza los cambios pertinentes 
+     * para que se actualicen todos los datos que luego se muestra en el display de nuestro reloj.
      */
     public void timeTick()
     {
         minutos.increment();
         if (minutos.getValue() == 0) {
             horas.increment();
+            if (horas.getValue() == 0) {
+                dia.increment();
+                if (dia.getValue() == 0){
+                    dia.increment();
+                    mes.increment();
+                    if (mes.getValue() == 0) {
+                        mes.increment();
+                        year.increment();
+                    }
+                }
+            }
         }
         modificarDisplay();
     }
     
     /**
-     * Este método hace que nuestro reloj funcione como un reloj en formato de 12 horas en vez de 24.
+     * Este método hace que nuestro reloj, funcione como un reloj en formato de 12 horas o de 24 según lo
+     * elijamos a la hora de crearlo en nuestro constructor y tambíen nos muestra la fecha.
      */
     public void modificarDisplay()
     {
-        if (formatoHora == true) {
-            String formato = "";
+        String fecha = dia.getDisplayValue() + "-" + mes.getDisplayValue() + "-" + year.getDisplayValue();
+        String nuevaHora = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + "  " + fecha;
+        if (formatoHora) {
+            String formato = "a.m.";
             int horaAhora = horas.getValue(); 
             if (horaAhora >= 12){
                 formato = "p.m.";
             }
-            else {
-                formato = "a.m.";
-            }
             if (horaAhora > 12) {
                 horaAhora = horaAhora - 12;
             }
-            if (horaAhora == 0) {
+            else if (horaAhora == 0) {
                 horaAhora = 12;
             }
-            horaActual = horaAhora + ":" + minutos.getDisplayValue() + " " + formato;
+            nuevaHora = horaAhora + ":" + minutos.getDisplayValue() + " " + formato + "  " + fecha;
         }
-        else {
-            horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue();
-        }
+        horaActual = nuevaHora;
     }
     
       /**
@@ -115,12 +150,7 @@ public class ClockDisplay
      */
     public void modifFormato()
     {
-        if (formatoHora == true) {
-            formatoHora = false;
-        }
-        else {
-            formatoHora = true;
-        }
+        formatoHora = !formatoHora;
         modificarDisplay();
     }
 }
